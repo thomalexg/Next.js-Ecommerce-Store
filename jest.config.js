ec9a3ac708ca8bdba18d625f4eabce1cb495139e
thomalexg <thomas.gae@posteo.de>
2021-03-04 16:49:04 +0100

    Added github actions

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+{
+  "testRegex": "(/__tests__/.*|(\\.|/)(test))\\.[jt]sx?$",
+  "name": "my-project",
+  "jest": {
+    "verbose": true
+  }
+"testRegex": "(/__tests__/.*|(\\.|/)(test))\\.[jt]sx?$"
