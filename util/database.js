ac3f65bd5548f0ab61d40d6a76daa6d06babcd33
thomalexg <thomas.gae@posteo.de>
2021-03-05 16:37:29 +0100

    Followed heroku cheatsheet

--- util/database.js
+++ util/database.js
@@ -9,11 +9,15 @@ require('dotenv-safe').config();
 
 // Connect only once to the database
 // https://github.com/vercel/next.js/issues/7811#issuecomment-715259370
+
 function connectOneTimeToDatabase() {
   let sql;
 
   if (process.env.NODE_ENV === 'production') {
-    sql = postgres({ ssl: false });
+    // Heroku needs SSL connections but
+    // has an "unauthorized" certificate
+    // https://devcenter.heroku.com/changelog-items/852
+    sql = postgres({ ssl: { rejectUnauthorized: false } });
   } else {
     if (!globalThis.__postgresSqlClient) {
       globalThis.__postgresSqlClient = postgres();
@@ -22,9 +26,6 @@ function connectOneTimeToDatabase() {
   }
   return sql;
 }
-
-// Connect to PostgreSQL
-const sql = connectOneTimeToDatabase();
 
 function camelcaseRecords(records) {
   return records.map((record) => camelcaseKeys(record));
@@ -35,6 +36,9 @@ export async function getProducts() {
   `;
   return camelcaseRecords(products);
 }
+
+// Connect to PostgreSQL
+const sql = connectOneTimeToDatabase();
 
 export async function getProduct(productId) {
   const product = await sql`
